6c50884d46d160eb482f592b491b26848b39d643
Matija Jandri <zudemain@gmail.com>
2025-09-10 15:05:28 +0200

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ErstellenComponent} from './erstellen-component/erstellen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'erstellen', component: ErstellenComponent },
+  { path: '', redirectTo: 'erstellen', pathMatch: 'full' },
+];
---
+++ src/app/erstellen-component/erstellen.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'erstellen-component',
+  imports: [RouterOutlet],
+  templateUrl: './erstellen.component.html',
+  styleUrl: './erstellen.component.css'
+})
+export class ErstellenComponent {
+}
